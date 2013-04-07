ff3a9b4f489301d3ca953f9205bc18229f9be53c
hongymagic <d.hong@me.com>
2013-04-07 10:39:09 +1000

    change bundle test structure as only the input sources differ

--- test/bundle.js
+++ test/bundle.js
@@ -2,38 +2,25 @@ var test = require('tap').test;
 var browserify = require('browserify');
 var vm = require('vm');
 
-test('bundle transform', function (t) {
-    t.plan(1);
+function bundle (file) {
+    test('bundle transform', function (t) {
+        t.plan(1);
 
-    var b = browserify();
-    b.add(__dirname + '/../example/foo.coffee');
-    b.transform(__dirname + '/..');
-    b.bundle(function (err, src) {
-        if (err) t.fail(err);
-        vm.runInNewContext(src, {
-            console: { log: log }
+        var b = browserify();
+        b.add(__dirname + '/../example/foo.coffee');
+        b.transform(__dirname + '/..');
+        b.bundle(function (err, src) {
+            if (err) t.fail(err);
+            vm.runInNewContext(src, {
+                console: { log: log }
+            });
         });
+
+        function log (msg) {
+            t.equal(msg, 555);
+        }
     });
+}
 
-    function log (msg) {
-        t.equal(msg, 555);
-    }
-});
-
-test('literate cofeescript bundle transform', function (t) {
-    t.plan(1);
-
-    var b = browserify();
-    b.add(__dirname + '/../example/foo.litcoffee');
-    b.transform(__dirname + '/..');
-    b.bundle(function (err, src) {
-        if (err) t.fail(err);
-        vm.runInNewContext(src, {
-            console: { log: log }
-        });
-    });
-
-    function log (msg) {
-        t.equal(msg, 555);
-    }
-});
+bundle('/../example/foo.coffee');
+bundle('/../example/foo.litcoffee');
